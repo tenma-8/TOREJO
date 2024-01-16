82b696d61327f13ab200be8757431b70a877648d
tenma-8 <sy021004@st.nuis.jp>
2024-01-16 11:33:58 +0900

    ダークモード変更

--- resources/views/body_records/body.blade.php
+++ resources/views/body_records/body.blade.php
@@ -8,15 +8,15 @@
         <title>Torejo</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.js"></script>
 
     </head>
-    </x-slot>
+   </x-slot>
     
-    <body>
+    <body style="background-color: white;">
         <h1>今日のあなたを記録しましょう！！</h1>
         <form action="/body_records" method="POST">
         　　 @csrf
